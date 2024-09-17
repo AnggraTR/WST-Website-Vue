dcb80f0e150efa43f55c06f32ac128386b9a63aa
anggra <anggra@unbin.ac.id>
2024-09-17 14:03:05 +0000

    Layout Website

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <body>
+    <header>Header</header>
+    <navbar>Main Menu</navbar>
+    <main>konten</main>
+    <footer>Footer</footer>
+  </body>
+</template>
+
+
